eadd5dfaa8ec62f7c94c99d8a2a800095d1d9610
happyyongku <yonggu1951@gmail.com>
2024-04-24 22:06:00 +0900

    upload

--- fortress/fortress.py
+++ fortress/fortress.py
@@ -8,13 +8,13 @@ from classes import Player, Button
 
 pygame.init() # pygame 모듈 초기화
 # 클래스 영역 ////////////////////////////////////////////////
-
+# 플레이어 클래스
 class Player:
     
     def __init__(self, initial_position, side):
         self.position = initial_position
         self.name = 'player' + str(side)
-        self.damage = 100
+        self.damage = 10
         self.volume = 200  #74
         self.side = side
         self.hp = 100
@@ -33,13 +33,14 @@ class Player:
         self.body[0] += dx
         self.body[1] += dy
         self.moved += abs(dx)
+        
     def hit(self, damage, scale):
         self.hp = round(self.hp - (damage * scale))
         idx = self.hp // 20
-        if self.hp % 20 != 0:
-            self.hp_img[idx] = img_hp[5-(self.hp % 20)//6]
-        else:
-            self.hp_img[idx] = img_hp[6]
+        # if self.hp % 20 != 0:
+        #     self.hp_img[idx] = img_hp[5-(self.hp % 20)//6]
+        # else:
+        #     self.hp_img[idx] = img_hp[6]
 
     
     def angle_move(self, theta):
@@ -53,11 +54,14 @@ class Player:
     
     def moved_init(self):
         self.moved = 0
+
+# 환경 클래스
 class Environment:
     season = 'spring'
     def __init__(self):
         self.element(self.season)
-        
+    
+    #turn을 인자로 받아 계절 계산
     def season_check(self, turn):
         if 1 <= turn < 4:
             self.season = 'spring'
@@ -68,33 +72,34 @@ class Environment:
         else:
             self.season = 'winter'
         self.element(self.season)
+        
+    # 계절을 인자로 받아 풍속, 풍향, 저항, 데미지 스케일 조정
     def element(self, season):
         if season == 'spring':
-            wind_velocity = list(range(10))
+            wind_velocity = list(range(11))
             wind_angle = list(range(0, 190, 10))
             self.resistance = 0
             self.damage_scale = 1
         elif season == 'summer':
-            wind_velocity = list(range(20))
+            wind_velocity = list(range(21))
             wind_angle = list(range(0, 190, 10))
             self.resistance = 0.2
             self.damage_scale = 0.7
         elif season == 'autumn':
-            wind_velocity = list(range(10))
+            wind_velocity = list(range(11))
             wind_angle = list(range(0, 190, 10))
             self.resistance = 0
             self.damage_scale = 2
         elif season == 'winter':
-            wind_velocity = list(range(20))
+            wind_velocity = list(range(21))
             wind_angle = list(range(0, 190, 10))
             self.resistance = 0.1
             self.damage_scale = 1.2
             
         self.v_w = wind_velocity[np.random.randint(0, len(wind_velocity)-1)]
         self.theta_w = wind_angle[np.random.randint(0, len(wind_angle)-1)]
-    
-    
-    
+
+# 버튼 클래스
 class Button:  # 버튼
     def __init__(self, img_in, x, y, width, height, img_act, x_act, y_act, action=None):
         mouse = pygame.mouse.get_pos()  # 마우스 좌표
@@ -111,23 +116,39 @@ class Button:  # 버튼
 # 설명 부분
 def explain():
     exp = True
-
+    tip = False
+    def switch():
+        # 로컬 변수로 tip 받아서 반대로 변환
+        nonlocal tip
+        if tip:
+            tip = False
+        else:
+            tip = True
+        return tip
     while exp:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        gameDisplay.blit(bg_explain, (0, 0))
-        Button(back, 500, 550, 230, 140, back_click, 500, 550, intro)
-
+        # tip이 켜지면 tip 페이지로, 아니면 설명 페이지로 출력
+        if not tip:
+            gameDisplay.blit(bg_explain, (0, 0))
+            Button(right_button, 1150, 350, 100, 100, right_button, 1150, 350, switch)
+        else:
+            gameDisplay.blit(bg_tip, (0, 0))
+            Button(left_button, 50, 350, 100, 100, left_button, 50, 350, switch)
+            
+        Button(back, 300, 600, 230, 140, back_click, 300, 600, intro)
+        Button(mainmenu_start, 700, 600, 230, 140, mainmenu_start_click, 700, 600, ready)
+        
         pygame.display.update()
         clock.tick(15)
 
 # 메인페이지(처음)
 def intro():
-    global gameDisplay, player1, player2, winner, turn, environment
+    global gameDisplay, player1, player2, win, defeated, turn, environment
     # 초기화
-    player1, player2, winner, environment = None, None, None, None
+    player1, player2, win, defeated, environment = None, None, None, None, None
     menu = True
     turn = 1
     clock = pygame.time.Clock()
@@ -180,13 +201,16 @@ def ready():
 
         Button(p1_button, 350, 550, 300, 50, p1_button_click, 350, 550, select1)
         Button(p2_button, 630, 550, 300, 50, p2_button_click, 630, 550, select2)
+        # 두 플레이어가 모두 준비되면 trigger를 올리기
         if player1 and player2:
             trigger = True
-
+        # 트리거가 켜지면 game 함수 실행
         if trigger:
             game(player1, player2)
         pygame.display.update()
         clock.tick(7)
+
+        # 체크용
         if not player1 and not player2:
             print('----------------------------------------------')
         elif player1:
@@ -210,12 +234,29 @@ def select2():
 
 
 # 게임이 끝났다면
-def game_over(winner):
+def game_over(win, defeated):
     game_over = True
     bg_main = [pygame.image.load(f"./ending_img/{i}.png") for i in range(147)]
     # 화면에 맞게 이미지 크기 조정
     bg_main = [pygame.transform.scale(image, (display_width, display_height)) for image in bg_main]
-    txt_1 = font_1.render(winner, True, WHITE)
+    
+    # 문구 출력용
+    text_1 = f'마침내 {win}의 진심이 통했다...'
+    text_2 = f'{win}의 열렬한 구애에 {defeated}의 철벽은 속절없이 함락당하고 말았다.'
+    text_3 = f'영원한 사랑의 노예가 되어버린 {defeated}...'
+    text_4 = f'하지만 걱정 마라. 사랑은 이기고 지는 게 아니니까.'
+
+    win_text_1 = korean_font.render(text_1, True, WHITE)
+    win_text_2 = korean_font.render(text_2, True, WHITE)
+    win_text_3 = korean_font.render(text_3, True, WHITE)
+    win_text_4 = korean_font.render(text_4, True, WHITE)
+    
+    win_rect_1 = win_text_1.get_rect(center = (640, 200))
+    win_rect_2 = win_text_2.get_rect(center = (640, 300))
+    win_rect_3 = win_text_3.get_rect(center = (640, 400))
+    win_rect_4 = win_text_4.get_rect(center = (640, 500))
+    
+    
     cur_idx = 0
     while game_over:
         for event in pygame.event.get():
@@ -227,8 +268,12 @@ def game_over(winner):
         time.sleep(0.1)
         # next_level += 500  # 3초 추가
         gameDisplay.blit(bg_main[display_idx], (0, 0))
-        gameDisplay.blit(txt_1,[40,450])
-        Button(mainmenu_start, 300, 600, 150, 80, mainmenu_start_click, 300, 600, intro)
+        gameDisplay.blit(ending_bg, ending_rect)
+        gameDisplay.blit(win_text_1, win_rect_1)
+        gameDisplay.blit(win_text_2, win_rect_2)
+        gameDisplay.blit(win_text_3, win_rect_3)
+        gameDisplay.blit(win_text_4, win_rect_4)
+        Button(regame_button, 500, 550, 300, 50, regame_button_click, 500, 550, intro)
         pygame.display.update() # 화면 업데이트
         cur_idx += 1
         clock.tick(15) #프레임 레이트 지정
@@ -239,14 +284,15 @@ def game(player1, player2):
     pygame.init()
     print(player1.position, player2.position)
     
+    # 시작하고 초기 환경 설정
     environment = Environment()
-    print(environment.v_w)
-    # font = pygame.font.Font(None, 40)
+    
     gameDisplay = pygame.display.set_mode((display_width, display_height))
     pygame.display.set_caption("대포 움직이기")
     clock = pygame.time.Clock()
     menu = True
     while menu:
+        # 현재 환경에 따라 배경 이미지 로드
         if environment.season == 'spring':
             background = pygame.image.load("./img/spring_bg.png")
         elif environment.season == 'summer':
@@ -256,6 +302,7 @@ def game(player1, player2):
         elif environment.season == 'winter':
             background = pygame.image.load("./img/winter_bg.png")
         
+        # 턴에 따라 플레이할 플레이어 선정
         if turn % 2 == 1:
             player = player1
         else:
@@ -267,13 +314,14 @@ def game(player1, player2):
                 sys.exit()
         keys = pygame.key.get_pressed()
 
+        # 방향키로 조정
         if keys[pygame.K_SPACE] :
             player.charge(5)
             if player.gauge >= 100 :
                 player.gauge = 100
         else :
-            if player.gauge != 0 :
-                shot(player)
+            if player.gauge > 0 :
+                shot(player)        # 현재 플레이어의 게이지가 0이 초과라면 shot 함수 실행
             player.gauge = 0
         if keys[pygame.K_UP]:
             if player.side == 1 and player.angle < 90:
@@ -286,12 +334,13 @@ def game(player1, player2):
             elif player.side == 2 and player.angle < 180:
                 player.angle_move(-1)
         if keys[pygame.K_RIGHT]:
-            if player.moved < 100:
+            if player.moved < 50:
                 player.move(5, 0)
         elif keys[pygame.K_LEFT] :
-            if player.moved < 100:
+            if player.moved < 50:
                 player.move(-5, 0)
 
+        # 화면 출력
         txt = font.render(str(player.gauge),True, BLACK)
         txt_angle_1 = font.render(str(player1.angle), True, BLACK)
         txt_angle_2 = font.render(str(180 - player2.angle), True, BLACK)
@@ -311,6 +360,9 @@ def game(player1, player2):
 
         gameDisplay.blit(background, (0, 0))  # 배경 이미지
         # gameDisplay.fill(WHITE)   # test 용
+        
+        gameDisplay.blit(character1, (player1.position[0]-100, player1.position[1]-80))
+        gameDisplay.blit(character2, (player2.position[0]-20, player2.position[1]-80))
 
         gameDisplay.blit(rotated_image1, new_rect1)  # 회전한 대포
         gameDisplay.blit(cannon_wheel,player1.position)  # 바퀴 이미지
@@ -329,8 +381,6 @@ def game(player1, player2):
         gameDisplay.blit(rotate_arrow, arrow_rect)
 
         # 캐릭터 이미지 출력
-        gameDisplay.blit(character1, (player1.position[0]-70, player1.position[1]-65))
-        gameDisplay.blit(character2, (player2.position[0]-30, player2.position[1]-65))
 
         # 체력 이미지 출력 파트
         temp_1 = (player1.hp-1)//20
@@ -376,18 +426,22 @@ def game(player1, player2):
                     gameDisplay.blit(img_hp[4], (pos_x, 100))
                 else:
                     gameDisplay.blit(img_hp[5], (pos_x, 100))
-        if winner:
+        # 전역 변수의 win이 있다면 while문 종료
+        if win:
             break
         pygame.display.update()
         clock.tick(20)
-
-    game_over(winner)
+    # game_over 함수 실행
+    game_over(win, defeated)
 
 # 발사 이미지 출력 함수
 def shot(player):
     global turn, font, player1, player2, environment
     
+    # environment 클래스의 풍속, 풍향, 저항, 데미지 스케일 받아오기
     v_w, theta_w, k, scale = environment.v_w, environment.theta_w, environment.resistance, environment.damage_scale
+    
+    # 플레이어 클래스의 게이지, 각도와 풍속, 풍향, 저항을 사용하여 x 좌표 리스트와 y좌표 리스트 추출
     x_coord, y_coord = coord(player, v_w, theta_w, k)
 
     txt_angle_1 = font.render(str(player1.angle), True, BLACK)
@@ -400,6 +454,7 @@ def shot(player):
     print(f'position : {player.position}')
     print(f'{x_coord[0], y_coord[0]}')
     
+    # 배경이미지
     season = seasonal(turn)
     if season == 'spring':
         background = pygame.image.load("./img/spring_bg.png")
@@ -410,8 +465,6 @@ def shot(player):
     elif season == 'winter':
         background = pygame.image.load("./img/winter_bg.png")
 
-    shell = pygame.image.load("./img/neko6.png")
-    # font = pygame.font.Font(None, 40)
     txt = font.render(str(player.gauge),True, BLACK)
     if player.side == 1:
         txt_angle = font.render(str(player.angle), True, BLACK)
@@ -423,8 +476,12 @@ def shot(player):
 
     rotated_image2 = pygame.transform.rotate(cannon_body2, player2.angle-180)
     new_rect2 = rotated_image2.get_rect(center=cannon_body2.get_rect(center=player2.body).center)
+    
+    rotate_arrow = pygame.transform.rotate(wind_direction, -environment.theta_w)
+    arrow_rect = rotate_arrow.get_rect(center = (640, 100))
 
     # 좌표에 따른 이미지 출력 부분
+    # idx를 하나씩 올리며 포탄 이미지 출력
     idx = 0
     while idx < len(x_coord):
         # 발사
@@ -434,6 +491,9 @@ def shot(player):
         # gameDisplay.fill(WHITE)
         
         gameDisplay.blit(shell, (x_coord[idx], y_coord[idx]))
+        
+        gameDisplay.blit(character1, (player1.position[0]-100, player1.position[1]-80))
+        gameDisplay.blit(character2, (player2.position[0]-20, player2.position[1]-80))
         
         gameDisplay.blit(rotated_image1, new_rect1)  # 회전한 대포1
         gameDisplay.blit(cannon_wheel,player1.position)  # 바퀴 이미지
@@ -447,12 +507,9 @@ def shot(player):
         gameDisplay.blit(txt,(0,0))
         
         # 캐릭터 이미지 출력
-        gameDisplay.blit(character1, (player1.position[0]-70, player1.position[1]-65))
-        gameDisplay.blit(character2, (player2.position[0]-30, player2.position[1]-65))
         
         # 풍향 출력
-        rotate_arrow = pygame.transform.rotate(wind_direction, -environment.theta_w)
-        arrow_rect = rotate_arrow.get_rect(center = (640, 100))
+        
         gameDisplay.blit(rotate_arrow, arrow_rect)
         
         gameDisplay.blit(txt_velocity, velocity_rect)
@@ -502,30 +559,38 @@ def shot(player):
         pygame.display.update()
         idx += 1
         clock.tick(200)
+    
+    # 마지막 x 좌표와 y 좌표가 impact
     impact = (x_coord[idx-1], y_coord[idx-1])
     
+    # 현재 플레이어와 impact, 계절의 데미지 스케일로 피격 판정
     calculate(player, impact, scale)
+    
+    # 그 후 턴 증가
     turn += 1
+    # 턴에 따라 환경의 계절 조정
     environment.season_check(turn)
+    # 플레이어가 움직였던 거리 초기화
     player.moved_init()
     
     
 
 # 피격 판정 계산 함수
 def calculate(player, impact, scale):
-    global player1
-    global player2
-    global winner
-    
+    global player1, player2, win, defeated
+    # 현재 플레이어의 side에 따라 상대 지정
     if player.side == 1:
         enemy = player2
     elif player.side == 2:
         enemy = player1
     
+    # 충돌 판정
     if impact[0] - enemy.volume <= enemy.position[0] <= impact[0] + enemy.volume:
         enemy.hit(player.damage, scale)
+        # 맞고 hp가 0 이하가 되면 승자와 패자 결정
         if enemy.hp <= 0:
-            winner = player.name
+            win = player.name
+            defeated = enemy.name
 
 
 # 변수 영역 //////////////////////////////////////////////////
@@ -546,25 +611,29 @@ back_click = pygame.image.load("./img/back_click.png")
 
 font = pygame.font.Font(None,60)
 font_1 = pygame.font.Font(None,100)
+korean_font = pygame.font.Font("./font/malgun.ttf", 30)
 
-wind_direction = pygame.transform.scale(pygame.image.load("./img/arrow.png"), (100, 100))
+wind_direction = pygame.transform.scale(pygame.image.load("./img/arrow2.png"), (100, 100))
 
-character1 = pygame.transform.scale(pygame.image.load("./img/character0.png"), (150, 150))
-character2 = pygame.transform.scale(pygame.image.load("./img/character1.png"), (150, 150))
+character1 = pygame.transform.scale(pygame.image.load("./img/character0.png"), (170, 170))
+character2 = pygame.transform.scale(pygame.image.load("./img/character1.png"), (170, 170))
 
 # 계절 이미지 변수
 summer_bg = pygame.image.load("./img/summer_bg.png")
 
-cannon_body1 = pygame.image.load("./img/cannon-3.png")
-cannon_body2 = pygame.image.load("./img/cannon-4.png")
+cannon_body1 = pygame.transform.scale(pygame.image.load("./img/cannon-3.png"), (118, 150))
+cannon_body2 = pygame.transform.scale(pygame.image.load("./img/cannon-4.png"), (118, 150))
 cannon_wheel = pygame.image.load("./img/cannon-1.png") # 24
 bomb = pygame.image.load("./img/heart_bomb.png")
 # wheel = [100,300]
 body = [124, 324]
 
+shell = pygame.image.load("./img/heart_bomb.png")
+
 player1 = None
 player2 = None
-winner = None
+win = None
+defeated = None
 environment = None
 turn = 1
 
@@ -591,9 +660,19 @@ pygame.display.set_caption("LOVE BOMB")  # 타이틀
 clock = pygame.time.Clock() #Clock 오브젝트 초기화
 
 bg_explain = pygame.image.load("./img/explain_bg.png")
+bg_tip = pygame.image.load("./img/intro_game_tip.png")
 bg = pygame.image.load("./img/neko_bg.png")
 bg_ready = pygame.image.load("./img/ready_bg.png")
 cursor =pygame.image.load("./img/neko_cursor.png")
+ending_bg = pygame.image.load("./img/game_over_result.png")
+
+ending_rect = ending_bg.get_rect(center = (640, 360))
+
+right_button = pygame.image.load("./img/intro_right_button.png")
+left_button = pygame.image.load("./img/intro_left_button.png")
+
+regame_button = pygame.image.load("./img/regame.png")
+regame_button_click = pygame.image.load("./img/regame_click.png")
 
 # 실행 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
